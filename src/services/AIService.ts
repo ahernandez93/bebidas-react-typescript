d94163a249c2fb048576003cf37f89568e9a1fed
Allan <allan_hc@outlook.com>
2026-03-09 15:52:18 -0600

    cambio de modelo IA

--- src/services/AIService.ts
+++ src/services/AIService.ts
@@ -4,8 +4,8 @@ import { openrouter } from '../lib/ai'
 export default {
     async generateRecipe(prompt: string) {
         const result = await streamText({
-            model: openrouter.chat('google/gemm a-3-4b-it:free'),
-            // model: openrouter.chat('qwen/qwen3-vl-30b-a3b-thinking'),
+            // model: openrouter.chat('google/gemm a-3-4b-it:free'),
+            model: openrouter.chat('qwen/qwen3-vl-30b-a3b-thinking'),
             // model: openrouter.chat('z-ai/glm-4.5-air:free'),
             prompt
         })
